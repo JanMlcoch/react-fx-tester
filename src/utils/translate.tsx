e0ea505fb10581b7950e665038e32f112a85cb0b
Jan Mlčoch <mlcoch.jan@gmail.com>
2019-06-10 14:42:28 +0200

    Fix typo

--- src/utils/translate.tsx
+++ src/utils/translate.tsx
@@ -1,4 +1,4 @@
-import React, {Component, ComponentType, FunctionComponent} from 'react'
+import React, {Component} from 'react'
 
 export interface WithTranslateProps {
   t: (term: string) => string;
