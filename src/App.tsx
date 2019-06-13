b253aa099e555bb578a17dd42c7bdf6640e5a7f5
Jan Mlčoch <mlcoch.jan@gmail.com>
2019-06-13 08:37:04 +0200

    Rename contact to user route

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,51 @@
 import React from 'react'
 import './App.css'
-import { Trader } from './components/trader/Trader'
-import { NavLink, Route, BrowserRouter as Router } from 'react-router-dom'
-import { Contact } from './components/contact/Contact'
+import {Trader} from './components/trader/Trader'
+import {NavLink, Route, BrowserRouter as Router} from 'react-router-dom'
+import {User} from './components/user/User';
 
 function App() {
-	return (
-		<Router>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<a className="navbar-brand" href="#">Fx tester</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse"
-				        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-				        aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<div className="navbar-nav">
-						<NavLink to="/" activeClassName="active" className="nav-item nav-link" href="#">Trader <span className="sr-only">(current)</span></NavLink>
-						<NavLink to="/contact" activeClassName="active" className="nav-item nav-link" href="#">Contact</NavLink>
-					</div>
-				</div>
-			</nav>
-			<div className="app container">
-				<Route path="/" exact component={Trader} />
-				<Route path="/contact/" component={Contact} />
-			</div>
-		</Router>
-	)
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Fx tester</a>
+        <button
+          className="navbar-toggler"
+          type="button" data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink
+              to="/"
+              activeClassName="active"
+              className="nav-item nav-link"
+              href="#"
+            >
+              Trader
+            </NavLink>
+            <NavLink
+              to="/user"
+              activeClassName="active"
+              className="nav-item nav-link"
+              href="#"
+            >
+              User
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+      <div className="app container">
+        <Route path="/" exact component={Trader}/>
+        <Route path="/user/" component={User}/>
+      </div>
+    </Router>
+  )
 }
 
 export default App
