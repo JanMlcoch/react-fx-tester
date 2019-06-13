c4f162cbcc4ec974d27732885327e68933094a07
Jan Mlčoch <mlcoch.jan@gmail.com>
2019-06-13 08:50:59 +0200

    Update translate fn

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
+const en = {
+	['Zavřít']: 'Close'
+};
+
+const cz = {
+	['Zavřít']: 'Zavřít'
+};
+
+const lang = 'en';
+// const lang = 'cz';
+
 export function connectTranslate(Component) {
 	const translate = (term) => {
-		const lib = {
-			'Zavřít': 'Close'
-		};
-		return lib[term];
+		const lib = lang.toString() === 'cz' ? cz : en;
+		return lib[term] ? lib[term] : term;
 	}
 
 	return function(props) {
